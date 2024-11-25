db6babac60f6955062d725326202b3ea11ce1829
EstyS637 <38328183660@mby.co.il>
2024-11-25 13:40:37 +0200

    home work with transaction

--- ‏‏ex2 - עותק/ex2/Repositories/RepositoryWithAdo.cs
+++ ‏‏ex2 - עותק/ex2/Repositories/RepositoryWithAdo.cs
@@ -17,7 +17,7 @@ namespace ex2.Repositories
         }
 
         //ADO - EX4 - PROCEDURE
-        public DataTable addAttachment(string Route, string AttachmentName, string Description, string size, string endingAttachment)
+        public DataTable addAttachment(string Route, string AttachmentName, string Description, string Size, string EndingAttacment)
         {
             DataTable dt = new DataTable();
             using (SqlConnection connection = new SqlConnection(Cnn))
@@ -31,8 +31,8 @@ namespace ex2.Repositories
                     SqlParameter sqlParameter1 = new SqlParameter("@Route", Route);
                     SqlParameter sqlParameter2 = new SqlParameter("@AttachmentName", AttachmentName);
                     SqlParameter sqlParameter3 = new SqlParameter("@Description", Description);
-                    SqlParameter sqlParameter4 = new SqlParameter("@size", size);
-                    SqlParameter sqlParameter5 = new SqlParameter("@endingAttachment", endingAttachment);
+                    SqlParameter sqlParameter4 = new SqlParameter("@Size", Size);
+                    SqlParameter sqlParameter5 = new SqlParameter("@EndingAttacment", EndingAttacment);
                     command.Parameters.Add(sqlParameter1);
                     command.Parameters.Add(sqlParameter2);
                     command.Parameters.Add(sqlParameter3);
@@ -71,6 +71,61 @@ namespace ex2.Repositories
                 }
             }
             return dt;
+        }
+
+
+        //transaction
+        public bool transaction_AddingTaskAndAttachment(AttachmentAndTask attachmentAndTask)
+        {
+            using (SqlConnection connect = new SqlConnection(Cnn))
+            {
+
+                connect.Open();
+
+                SqlTransaction transactionAddingAttachmentAndTasks = connect.BeginTransaction();
+
+                try
+                {
+                    using (SqlCommand command = new SqlCommand("insert into Attachments (Route,Description,Size,EndingAttacment,AttachmentName)" + "values(@Route, @Description, @Size, @EndingAttacment, @AttachmentName)", connect, transactionAddingAttachmentAndTasks))
+                    {
+                        command.Parameters.AddWithValue("@Route", attachmentAndTask.attachment.Route);
+                        command.Parameters.AddWithValue("@Description", attachmentAndTask.attachment.Description);
+                        command.Parameters.AddWithValue("@Size", attachmentAndTask.attachment.Size);
+                        command.Parameters.AddWithValue("@EndingAttacment", attachmentAndTask.attachment.EndingAttacment);
+                        command.Parameters.AddWithValue("@AttachmentName", attachmentAndTask.attachment.AttachmentName);
+                        command.ExecuteNonQuery();
+
+
+                    }
+                    
+
+                    using (SqlCommand command = new SqlCommand("insert into Tasks (Priority,DueDate,Status,ProjectId,UserId)" + "values(@Priority, @DueDate, @Status, @ProjectId, @UserId)", connect, transactionAddingAttachmentAndTasks))
+                    {
+                        command.Parameters.AddWithValue("@Priority", attachmentAndTask.task.Priority);
+                        command.Parameters.AddWithValue("@DueDate", attachmentAndTask.task.DueDate);
+                        command.Parameters.AddWithValue("@Status", attachmentAndTask.task.Status);
+                        command.Parameters.AddWithValue("@ProjectId", attachmentAndTask.task.ProjectId);
+                        command.Parameters.AddWithValue("@UserId", attachmentAndTask.task.UserId);
+                        command.ExecuteNonQuery();
+                    }
+
+                    transactionAddingAttachmentAndTasks.Commit();
+                    return true;
+                }
+
+                catch
+                {
+                    transactionAddingAttachmentAndTasks.Rollback();
+                    return false;
+                }
+
+
+
+
+
+
+
+            }
         }
     }
 }
